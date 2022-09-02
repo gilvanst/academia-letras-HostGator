fecf5568bd80977a7f2e15376bcaf4a01d633050
gilvanst <106268024+gilvanst@users.noreply.github.com>
2022-09-02 16:26:15 -0300

    não permitir campos vazios

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    
+
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
@@ -18,7 +18,7 @@
     <style>
         body {
             font-family: Arial, Helvetica, sans-serif;
-            
+
         }
 
         .bodyf {
@@ -47,13 +47,14 @@
             background-image: linear-gradient(to right, rgb(0, 80, 172), rgb(80, 19, 195));
             cursor: pointer;
         }
-
-     
     </style>
 </head>
 
+
 <body class="bg-gradient-primary">
     <div class="bodyf">
+
+
         <div class="text-center text-gray-700">
             <h1>Login</h1>
         </div>
@@ -62,7 +63,7 @@
                 <input class="form-control form-user" type="text" name="email" placeholder="Email" required>
             </div>
             <div class="form-group">
-                <input class="form-control form-user" type="password" name="senha" placeholder="Senha" required>
+                <input class="form-control form-user" type="password" name="senha" minlength="3" placeholder="Senha" required>
 
             </div>
 
