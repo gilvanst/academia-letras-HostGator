c1dbe9034fa68a8463b880e0c0373a2ce5e31d18
gilvanst <106268024+gilvanst@users.noreply.github.com>
2022-09-08 15:36:18 -0300

    voltei ao que estava pois estava dando erro

--- modulos/obras/alterar.php
+++ modulos/obras/alterar.php
@@ -2,20 +2,29 @@
 <html lang="pt-br">
 
 <?php
+
 include '../../config.php';
-
-if (empty($_SESSION['usuario'])) {
-    header('Location: ' . arquivo('index.php'));
-    exit;
-}
-
 if (!empty($_GET['idObra'])) {
     $id = $_GET['idObra'];
 }
 
-$sql = 'SELECT * FROM obra where idObra =';
-$obra = retornaDado($sql);
+$pdo = Banco::conectar();
+$sql = 'SELECT * FROM obra where idObra =  ' . $id . '';
 
+foreach ($pdo->query($sql) as $row) {
+    $id      = $row['idObra'];
+    $titulo  = $row['tituloObra'];
+    $autores = $row['autoresObra'];
+    $sinopse = $row['sinopseObra'];
+    $imagem  = $row['imagemObra'];
+    $isbn    = $row['isbnObra'];
+    $ano     = $row['anoObra'];
+    $paginas = $row['paginasObra'];
+    $pdf     = $row['pdfObra'];
+    $link    = $row['linkObra'];
+    $genero  = $row['generoObra'];
+}
+Banco::desconectar();
 ?>
 
 <?php include_once path('template/head.php'); ?>
@@ -49,59 +58,59 @@ $obra = retornaDado($sql);
                     <form action="update.php" method="POST">
                         <div class="form-group">
                             <label for="titulo">Titulo</label>
-                            <input class="form-control" type="text" id="titulo" name="tituloObra" value="<?php echo $obra['tituloObra']; ?>">
+                            <input class="form-control" type="text" id="titulo" name="tituloObra" value="<?php echo $row['tituloObra']; ?>">
                         </div>
 
                         <div class="form-group ">
                             <label for="autores">Autores</label>
-                            <input class="form-control" type="text" id="autores" name="autoresObra" value="<?php echo $obra['autoresObra']; ?>">
+                            <input class="form-control" type="text" id="autores" name="autoresObra" value="<?php echo $row['autoresObra']; ?>">
                         </div>
 
                         <div class="form-group ">
                             <label for="genero">Gênero</label>
-                            <input class="form-control" type="text" id="genero" name="generoObra" value="<?php echo $obra['generoObra']; ?>">
+                            <input class="form-control" type="text" id="genero" name="generoObra" value="<?php echo $row['generoObra']; ?>">
                         </div>
 
                         <div class="form-group ">
                             <label for="isbn">Isbn</label>
-                            <input class="form-control" type="text" id="isbn" name="isbnObra" value="<?php echo $obra['isbnObra']; ?>">
+                            <input class="form-control" type="text" id="isbn" name="isbnObra" value="<?php echo $row['isbnObra']; ?>">
                         </div>
 
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="ano">Ano de publicação</label>
-                                <input class="form-control " type="number" id="ano" name="anoObra" value="<?php echo $obra['anoObra']; ?>">
+                                <input class="form-control " type="number" id="ano" name="anoObra" value="<?php echo $row['anoObra']; ?>">
                             </div>
 
                             <div class="form-group col-md-6 ">
                                 <label for="paginas">Quantidade de páginas</label>
-                                <input class="form-control " type="number" id="paginas" name="paginasObra" value="<?php echo $obra['paginasObra']; ?>">
+                                <input class="form-control " type="number" id="paginas" name="paginasObra" value="<?php echo $row['paginasObra']; ?>">
                             </div>
 
                         </div>
                         <div class="form-row">
                             <div class="form-gorup col-md-6">
                                 <label for="imagem">Capa do livro</label>
-                                <input class="form-control-file" type="file" id="imagem" name="imagem" value="<?php echo $obra['imagemObra']; ?>">
+                                <input class="form-control-file" type="file" id="imagem" name="imagem" value="<?php echo $row['imagemObra']; ?>">
                             </div>
 
                             <div class="form-gorup col-md-6">
                                 <label for="pdf">Arquivo de leitura</label>
-                                <input class="form-control-file" type="file" id="pdf" name="pdfObra" value="<?php echo $obra['pdfObra']; ?>">
+                                <input class="form-control-file" type="file" id="pdf" name="pdfObra" value="<?php echo $row['pdfObra']; ?>">
                             </div>
                         </div>
 
                         <div class="form-gorup">
                             <label for="link">Link da obra</label>
-                            <input class="form-control" type="text" id="link" name="linkObra" value="<?php echo $obra['linkObra']; ?>">
+                            <input class="form-control" type="text" id="link" name="linkObra" value="<?php echo $row['linkObra']; ?>">
                         </div>
 
                         <div class="form-gorup">
                             <label for="sinopse">Sinopse</label>
-                            <textarea class="form-control" type="sinopse" id="sinopse" name="sinopseObra" value="<?php echo $obra['sinopseObra']; ?>"><?= $obra['sinopseObra'] ?></textarea>
+                            <textarea class="form-control" type="sinopse" id="sinopse" name="sinopseObra" value="<?php echo $row['sinopseObra']; ?>"><?= $row['sinopseObra'] ?></textarea>
                         </div>
 
-                        <input type="hidden" id="id" name="idObra" value="<?php echo $obra['idObra']; ?>">
+                        <input type="hidden" id="id" name="idObra" value="<?php echo $row['idObra']; ?>">
 
                         <div class="text-right my-3">
                             <input class="btn btn-primary" type="submit" value="Alterar">
