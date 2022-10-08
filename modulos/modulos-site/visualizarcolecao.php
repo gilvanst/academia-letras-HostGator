e5065a0d3f3ee375f8e214be2e4129c6c2d5f9d3
Bralozin <brulokony@gmail.com>
2022-10-07 22:26:44 -0300

    criei a tela de vizualizar coleção

---
+++ modulos/modulos-site/visualizarcolecao.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+
+<html>
+<?php
+include '../../config.php';
+?>
+
+<?php include_once path('template/template-site/head.php'); ?>
+<header id="header">
+	<div class="logo container">
+		<div>
+			<h1><a href="<?= arquivo('index.php')?>" id="logo"></a></h1>
+			<p> Coleção</p>
+		</div>
+	</div>
+</header>
+<body class="is-preload">
+	<div id="page-wrapper">
+
+		<?php include_once path('template/template-site/navbar.php'); ?>
+        <section id="main">
+			<div class="container">
+				<div class="row">
+                </div>
+            </div>        
+    </div>
+</body>    
+
+<footer id="footer">
+			<div class="container">
+				<div class="row gtr-200">
+				<?php include_once path('template/template-site/contato.php'); ?>
+					<div class="col-12">
+
+						<!-- About -->
+						<section>
+							<h2 class="major"><span>Desenvolvedores</span></h2>
+							<p>
+								Gilvan Santos <br>
+								Bruno Corrêa <br>
+								Caique Pollachini <br>
+								Kenjhon Leandro <br>
+							</p>
+						</section>
+
+					</div>
+					
+				</div>
+
+				
+
+			</div>
+		</footer>
+
+	</div>
+
+	<?php include_once path('template/template-site/importacoes-js.php'); ?>
+</body>
+
+</html>
