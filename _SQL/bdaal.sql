5aaa9efe686c6d0d83df2a89f25aac74418cd6e3
gilvanst <106268024+gilvanst@users.noreply.github.com>
2022-10-20 17:30:48 -0300

    att banco

--- _SQL/bdaal.sql
+++ _SQL/bdaal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Ago-2022 às 22:04
+-- Tempo de geração: 20-Out-2022 às 22:29
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -31,7 +31,8 @@ CREATE TABLE `academicos` (
   `idAcad` int(11) NOT NULL,
   `nomeAcad` varchar(150) NOT NULL,
   `cadeiraAcad` varchar(10) NOT NULL,
-  `posicaoAcad` varchar(50) NOT NULL
+  `posicaoAcad` varchar(50) NOT NULL,
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -46,7 +47,8 @@ CREATE TABLE `eventos` (
   `localEve` varchar(250) NOT NULL,
   `dataEve` date NOT NULL,
   `horaEve` time NOT NULL,
-  `descricaoEve` varchar(300) NOT NULL
+  `descricaoEve` varchar(300) NOT NULL,
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -66,18 +68,22 @@ CREATE TABLE `obra` (
   `paginasObra` varchar(5) NOT NULL,
   `pdfObra` varchar(200) NOT NULL,
   `linkObra` varchar(200) NOT NULL,
-  `generoObra` varchar(20) NOT NULL
+  `generoObra` varchar(20) NOT NULL,
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `obra`
 --
 
-INSERT INTO `obra` (`idObra`, `tituloObra`, `autoresObra`, `sinopseObra`, `imagemObra`, `isbnObra`, `anoObra`, `paginasObra`, `pdfObra`, `linkObra`, `generoObra`) VALUES
-(1, 'Harry Potter e a Pedra Filosofal', 'J.K rowling', 'Harry Potter é um garoto órfão que vive infeliz com seus tios, os Dursleys. Ele recebe uma carta contendo um convite para ingressar em Hogwarts, uma famosa escola especializada em formar jovens bruxos. Inicialmente, Harry é impedido de ler a carta por seu tio, mas logo recebe a visita de Hagrid, o guarda-caça de Hogwarts, que chega para levá-lo até a escola. Harry adentra um mundo mágico que jamais imaginara, vivendo diversas aventuras com seus novos amigos, Rony Weasley e Hermione Granger.', '', '8532511015', '2000', '264', '', 'https://www.amazon.com.br/Harry-Potter-Pedra-Filosofal-Rowling/dp/8532511015', 'Aventura e fantasia'),
-(2, 'algma', 'fsdsfd', '', '', '1201453', '210', '31235', 'Harry-Potter-e-a-pedra-filosofal.pdf', '', 'dfsdfsf'),
-(3, 'dsiclasjif', 'dfpifipsdk', '', 'harry2.jpg', '545215', '3512', '54534', '', '', 'fsjdksdf'),
-(4, 'fadsdfds', 'aadss', '', '', '', '', '', '', '', '');
+INSERT INTO `obra` (`idObra`, `tituloObra`, `autoresObra`, `sinopseObra`, `imagemObra`, `isbnObra`, `anoObra`, `paginasObra`, `pdfObra`, `linkObra`, `generoObra`, `id_usuario`) VALUES
+(83, 'As vantagens de ser invisível', 'Sthephen Chobsky', '\"O LIVRO QUE INSPIROU O FILMENOVA EDIÇÃO COM TRECHO INÉDITO Manter-se à margem oferece uma única e passiva perspectiva. Mas, de uma hora para outra, sempre chega o momento de encarar a vida do centro dos holofotes. Mais íntimas do que um diário, as cartas de Charlie são estranhas e únicas, hilárias e devastadoras. Não se sabe onde ele mora. Não se sabe para quem ele escreve. Tudo o que se conhece é o mundo que ele compartilha com o leitor. Estar encurralado entre o desejo de viver sua vida e fugir dela o coloca num novo caminho através de um território inexplorado. Um mundo de primeiros encontros amorosos, dramas familiares e novos amigos. Um mundo de sexo, drogas e rock’n’roll, quando o que todo mundo quer é aquela música certa que provoca o impulso perfeito para se sentir infinito. A luta entre apatia e entusiasmo marca o fim da adolescência de Charlie nesta história divertida e ao mesmo tempo instigante.\"', 'As vantagens de ser invisível1010202208105903.jpg', '9786555320695', '2014', '293', 'As vantagens de ser invisível1010202208105903.pdf', 'https://www.amazon.com.br/VANTAGENS-SER-INVIS%C3%8DVEL-NOVA-EDI%C3%87%C3%83O/dp/6555320699', 'romance, drama', 15),
+(86, 'Harry Potter e a pedra filosofal', 'J.K rowlling', 'Harry Potter é um garoto cujos pais, feiticeiros, foram assassinados por um poderosíssimo bruxo quando ele ainda era um bebê. Ele foi levado, então, para a casa dos tios que nada tinham a ver com o sobrenatural. Pelo contrário. Até os 10 anos, Harry foi uma espécie de gata borralheira: maltratado pelos tios, herdava roupas velhas do primo gorducho, tinha óculos remendados e era tratado como um estorvo. No dia de seu aniversário de 11 anos, entretanto, ele parece deslizar por um buraco sem fundo, como o de Alice no país das maravilhas, que o conduz a um mundo mágico. Descobre sua verdadeira história e seu destino: ser um aprendiz de feiticeiro até o dia em que terá que enfrentar a pior força do mal, o homem que assassinou seus pais. O menino de olhos verde, magricela e desengonçado, tão habituado à rejeição, descobre, também, que é um herói no universo dos magos. Potter fica sabendo que é a única pessoa a ter sobrevivido a um ataque do tal bruxo do mal e essa é a causa da marca em forma de raio que ele carrega na testa. Ele não é um garoto qualquer, ele sequer é um feiticeiro qualquer ele é Harry Potter, símbolo de poder, resistência e um líder natural entre os sobrenaturais. A fábula, recheada de fantasmas, paredes que falam, caldeirões, sapos, unicórnios, dragões e gigantes, não é, entretanto, apenas um passatempo.', 'Harry Potter e a pedra filosofal1710202205102033.jpg', '345644563456', '2004', '208', 'Harry Potter e a pedra filosofal1710202205102033.pdf', 'https://www.amazon.com.br/Harry-Potter-Pedra-Filosofal-Rowling/dp/8532530788', 'aventura', 15),
+(87, 'O pequeno príncipe', 'Antoíne de Saint-Exupery', 'Nesta clássica história que marcou gerações de leitores em todo o mundo, um piloto cai com seu avião no deserto do Saara e encontra um pequeno príncipe, que o leva a uma jornada filosófica e poética através de planetas que encerram a solidão humana. A edição conta com a clássica tradução do poeta imortal dom Marcos Barbosa, e é a versão mais consagrada da obra, publicada no Brasil desde 1952.', 'O pequeno príncipe1710202210101058.jpg', '8595081514', '2018', '96', 'O pequeno príncipe1710202210101058.pdf', 'https://www.amazon.com.br/pequeno-pr%C3%ADncipe-Antoine-Saint-Exup%C3%A9ry/dp/8595081514', 'ficção ciêntifica', 16),
+(88, 'Memórias póstumas de Brás Cubas', 'Machado De Assis', 'Não tive filhos, não transmiti a nenhuma criatura o legado da nossa miséria. Com essas palavras, o narrador de Memórias Póstumas de Brás Cubas resume a sua vida. O tom assumido na obra, bem como as técnicas empregadas na composição romanesca, são alguns dos fatores que justificam o lugar de Machado de Assis entre os maiores escritores do século XIX. Neste romance repleto de digressões filosóficas, o escritor se vale da posição privilegiada de Brás Cubas, que, como defunto autor, narra as suas desventuras e revela as contradições da sociedade brasileira do século XIX, por meio de uma análise aprofundada de seus personagens.', 'Memórias póstumas de Brás Cubas1810202206103036.jpg', '9788594318619', '1881', '192', 'Memórias póstumas de Brás Cubas1810202206103036.pdf', 'https://www.amazon.com.br/Mem%C3%B3rias-P%C3%B3stumas-Cubas-Machado-Assis/dp/8594318618/ref=d_pd_sbs_sccl_2_2/131-3698504-9154808?pd_rd_w=2JWqM&content-id=amzn1.sym.077f096c-3881-4160-87ce-2fdca6e1052', 'Literatura clássica', 15),
+(89, 'A metamorfose', 'Franz Kafka', 'A metamorfose é a mais célebre novela de Franz Kafka e uma das mais importantes de toda a história da literatura. Sem a menor cerimônia, o texto coloca o leitor diante de um caixeiro-viajante - o famoso Gregor Samsa - transformado em inseto monstruoso. A partir daí, a história é narrada com um realismo inesperado que associa o inverossímil e o senso de humor ao que é trágico, grotesco e cruel na condição humana - tudo no estilo transparente e perfeito desse mestre inconfundível da ficção universal.\r\n', 'A metamorfose1810202206104543.jpg', '139788571646858', '1915', '104', 'A metamorfose1810202206104543.pdf', 'https://www.amazon.com.br/metamorfose-Franz-Kafka/dp/8571646856', 'Ficçao absurdista', 15),
+(90, 'Aurora ', 'Friedrich Nietizche', 'Publicado em 1881, Aurora prossegue no estilo aforístico da filosofia de Friedrich Nietzsche (1844-1900), inaugurado com Humano, demasiado humano (1878). Em 575 aforismos - cuja extensão varia de duas linhas a algumas páginas -, Nietzsche elabora sua crítica da moral cristã-ocidental e dos conceitos a ela associados, como \"alma\", \"Deus\", \"pecado\", \"sujeito\" e \"livre-arbítrio\", que segundo o filósofo seriam ficções prejudiciais à vida. No subtítulo do livro, a palavra \"preconceito\" é usada no sentido filosófico de concepção formada antes do julgamento (um \"pré-juízo\"). A epígrafe (\"Há tantas auroras que não brilharam ainda\", extraído das escrituras hindus) explica o título e traduz a esperança nietzscheana de um mundo novo, livre das ilusões religiosas, morais e intelectuais criticadas pelo filósofo. Essa edição inclui o importante prólogo acrescentado em 1886 e, num apêndice bilíngüe, o grupo de poemas intitulados \"Idílios de Messina\", de 1882.', 'Aurora 1810202206105809.jpg', '8535928189', '1881', '312', 'Aurora 1810202206105809.pdf', 'https://www.amazon.com.br/Aurora-Friedrich-Nietzsche/dp/8535928189', 'Filosofia', 15),
+(92, 'The ousiders: Vidas sem rumo', 'S.E Hinton', 'Publicado pela primeira vez em 1967 e imortalizado em 1983 pelo filme de Francis Ford Coppola, The Outsiders: Vidas sem rumo é um clássico da literatura jovem que transformou o gênero ao tratar, com complexidade e sensibilidade, de uma juventude marginalizada em um cotidiano sombrio e violento.\r\n\r\n\r\nNa pequena cidade de Tulsa, em Oklahoma, a rivalidade entre dois grupos cresce a cada dia. De um lado estão os Greasers, com suas jaquetas de couro, o cabelo com brilhantina, os canivetes em punho e o cotidiano marcado pela falta de perspectiva e pelas brigas em terrenos baldios. Já os Socs moram no lado rico da cidade, frequentam os melhores lugares e conseguem se safar das piores situações, inclusive quando decidem espancar os Greasers.\r\n\r\n\r\nCaçula do grupo, Ponyboy Curtis tem apenas quatorze anos. Junto dos irmãos Darry e Sodapop e dos companheiros Greasers, ele tenta encontrar uma forma de escapar de uma vida sem futuro em um lugar onde tudo se resume a ser rico ou pobre. Apaixonado por cinema e por livros, Ponyboy encontra nessas histórias uma chance de conhecer outra realidade. Tudo parece sob controle, até que uma perseguição dos Socs tem um desfecho inesperado e perturbador. Nada será como antes, mas os Greasers são uma verdadeira família e farão de tudo para protegê-la.\r\n\r\n\r\nThe Outsiders: Vidas sem rumo é um retrato comovente de uma juventude que precisa encarar a solidão, a revolta e a violência em sua jornada de amadurecimento. Mais de cinco décadas após o lançamento do livro, a história ainda encanta jovens e adultos, mostrando-se atemporal. Com capa dura e pintura trilateral, além de tradução e projeto gráfico inéditos, a edição traz ainda prefácio da jornalista e crítica de cinema Ana Maria Bahiana, entrevista com a autora S.E. Hinton e uma seção dedicada aos bastidores do filme.', 'B086R7YJXQ.01._SCLZZZZZZZ_SX500_.jpg', '8551006622', '2020', '201', 'The ousiders: Vidas sem rumo1810202207100859.pdf', 'https://www.amazon.com.br/Outsiders-Vidas-Sem-Rumo-ebook/dp/B086R7YJXQ', 'literatura Jovem', 15);
 
 -- --------------------------------------------------------
 
@@ -99,7 +105,7 @@ CREATE TABLE `publicacoes` (
 --
 
 INSERT INTO `publicacoes` (`IdPub`, `tituloPub`, `autoresPub`, `generoPub`, `textoPub`, `id_usuario`) VALUES
-(11, 'zfssdfs', 'fdssf', 'fdsfdsf', 'dfsfds', 11);
+(15, 'O Menino Azul', 'Cecília Meireles', 'Poemas', 'O menino quer um burrinho\r\npara passear.\r\nUm burrinho manso,\r\nque não corra nem pule,\r\nmas que saiba conversar.\r\n\r\nO menino quer um burrinho\r\nque saiba dizer\r\no nome dos rios,\r\ndas montanhas, das flores,\r\n– de tudo o que aparecer.\r\n\r\nO menino quer um burrinho\r\nque saiba inventar histórias bonitas\r\ncom pessoas e bichos\r\ne com barquinhos no mar.\r\n\r\nE os dois sairão pelo mundo\r\nque é como um jardim\r\napenas mais largo\r\ne talvez mais comprido\r\ne que não tenha fim.\r\n\r\n(Quem souber de um burrinho desses,\r\npode escrever\r\npara a Ruas das Casas,\r\nNúmero das Portas,\r\nao Menino Azul que não sabe ler.)', 16);
 
 -- --------------------------------------------------------
 
@@ -120,8 +126,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `senha`) VALUES
-(11, 'adm', 'adm@adm', 123, '123'),
-(12, 'adm1', 'adm1@adm', 0, '123');
+(15, 'adm@adm', 'adm@adm', 123, '123'),
+(16, 'adm', 'adm1@adm', 123, '123');
 
 --
 -- Índices para tabelas despejadas
@@ -171,25 +177,25 @@ ALTER TABLE `academicos`
 -- AUTO_INCREMENT de tabela `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `idEve` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEve` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `obra`
 --
 ALTER TABLE `obra`
-  MODIFY `idObra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idObra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT de tabela `publicacoes`
 --
 ALTER TABLE `publicacoes`
-  MODIFY `IdPub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `IdPub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
