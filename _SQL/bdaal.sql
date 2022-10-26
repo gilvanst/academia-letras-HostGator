bcf175631fc66692f54e4b9e4dcd53f0e1dc67d1
gilvanst <106268024+gilvanst@users.noreply.github.com>
2022-10-26 15:05:11 -0300

    att banco

--- _SQL/bdaal.sql
+++ _SQL/bdaal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Out-2022 às 22:38
+-- Tempo de geração: 26-Out-2022 às 20:04
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
