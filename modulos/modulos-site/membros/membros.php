c8c1237a3049f237af3d19f3adfb5c73d74ec42b
Bralozin <brulokony@gmail.com>
2022-11-08 22:22:20 -0300

    Criei visualizar dos membros e arrumei a aba "membros"

--- modulos/modulos-site/membros/membros.php
+++ modulos/modulos-site/membros/membros.php
@@ -16,7 +16,48 @@ include '../../../config.php';
 
 <body class="homepage is-preload">
     <div id="page-wrapper">
+
         <?php include_once path('template/template-site/navbar.php'); ?>
+        
+        <section id="main">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<section class="box features">
+							<div>
+								<div class="row">
+									<?php
+									$sql = "SELECT * FROM academicos ORDER BY idAcad ASC";
+									$membros = retornaDados($sql);
+
+									foreach ($membros as $membro) { ?>
+										<div class="col-3 col-6-medium col-12-small">
+											<!-- Feature -->
+											<section class="box feature">
+												<a href="visualizar.php?idAcad=<?= $membro['idAcad'] ?>" class="image featured">
+													<img src="<?= arquivo('img/' . $membro['fotoAcad']) ?>" alt="" />
+
+												</a>
+												<h3><a class="titulo-hover" href="visualizar.php?idAcad=<?= $membro['idAcad'] ?>"><?= $membro['nomeAcad'] ?></a></h3>
+                                                <p>Posição: <?= $membro['posicaoAcad'] ?></p>
+                                                <p>Cadeira: <?= $membro['cadeiraAcad'] ?></p>
+												<p>
+													<?= substr($membro['sobreAcad'], 0, 200) ?>...
+												</p>
+											</section>
+										</div>
+
+									<?php } ?>
+
+								</div>
+							</div>
+							
+						</section>
+					</div>
+				</div>
+			</div>
+		</section>
+
         <section id="main">
             <div class="container">
                 <div class="row gtr-200">
