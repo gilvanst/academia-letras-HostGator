84c71f72d20ab2de25a90e0d7742146c51aed805
gilvanst <gilvanjr306@gmail.com>
2022-11-22 22:31:37 -0300

    algup

--- modulos/modulos-site/historia/historia.php
+++ modulos/modulos-site/historia/historia.php
@@ -25,47 +25,25 @@ include '../../../config.php';
                     <div class="col-12">
                         <div class="content">
                             <article class="box page-content">
+                            <?php
+                            $sql = "SELECT * FROM historia";
+
+                            $historias = retornaDados($sql);
+
+                            foreach ($historias as $historia) { ?>
 
                                 <section>
                                     <h3>Quem somos?</h3>
                                     <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac quam risus, at tempus
-                                        justo. Sed dictum rutrum massa eu volutpat. Quisque vitae hendrerit sem. Pellentesque lorem felis,
-                                        ultricies a bibendum id, bibendum sit amet nisl. Mauris et lorem quam. Maecenas rutrum imperdiet
-                                        vulputate. Nulla quis nibh ipsum, sed egestas justo. Morbi ut ante mattis orci convallis tempor.
-                                        Etiam a lacus a lacus pharetra porttitor quis accumsan odio. Sed vel euismod nisi. Etiam convallis
-                                        rhoncus dui quis euismod. Maecenas lorem tellus, congue et condimentum ac, ullamcorper non sapien
-                                        vulputate. Nulla quis nibh ipsum, sed egestas justo. Morbi ut ante mattis orci convallis tempor.
-                                        Etiam a lacus a lacus pharetra porttitor quis accumsan odio. Sed vel euismod nisi. Etiam convallis
-                                        rhoncus dui quis euismod. Maecenas lorem tellus, congue et condimentum ac, ullamcorper non sapien.
-                                        Donec sagittis massa et leo semper a scelerisque metus faucibus. Morbi congue mattis mi.
-                                        Phasellus sed nisl vitae risus tristique volutpat. Cras rutrum commodo luctus.
+                                        <?=$historia['texto']?>
                                     </p>
                                 </section>
 
-                                <section>
-                                    <h3>So in conclusion ...</h3>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac quam risus, at tempus
-                                        justo. Sed dictum rutrum massa eu volutpat. Quisque vitae hendrerit sem. Pellentesque lorem felis,
-                                        ultricies a bibendum id, bibendum sit amet nisl. Mauris et lorem quam. Maecenas rutrum imperdiet
-                                        vulputate. Nulla quis nibh ipsum, sed egestas justo. Morbi ut ante mattis orci convallis tempor.
-                                        Etiam a lacus a lacus pharetra porttitor quis accumsan odio. Sed vel euismod nisi. Etiam convallis
-                                        rhoncus dui quis euismod. Maecenas lorem tellus, congue et condimentum ac, ullamcorper non sapien.
-                                        Donec sagittis massa et leo semper a scelerisque metus faucibus. Morbi congue mattis mi.
-                                        Phasellus sed nisl vitae.
-                                    </p>
-                                    <p>
-                                        Suspendisse laoreet metus ut metus imperdiet interdum aliquam justo tincidunt. Mauris dolor urna,
-                                        fringilla vel malesuada ac, dignissim eu mi. Praesent mollis massa ac nulla pretium pretium.
-                                        Maecenas tortor mauris, consectetur pellentesque dapibus eget, tincidunt vitae arcu.
-                                    </p>
-                                </section>
 
                             </article>
 
                         </div>
-
+                        <?php } ?>
                     </div>
 
                 </div>
