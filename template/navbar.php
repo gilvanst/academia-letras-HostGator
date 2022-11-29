e4394217a98c79c7af9cf1b786896dadf538f39b
gilvanst <106268024+gilvanst@users.noreply.github.com>
2022-11-29 14:32:13 -0300

    tirei pesquisa de adm

--- template/navbar.php
+++ template/navbar.php
@@ -1,15 +1,6 @@
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
     <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-            <input type="text" class="form-control bg-light border-0 small" placeholder="Procurar por......" aria-label="Search" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
-            </div>
-        </div>
-    </form>
+
 
     <!-- Sidebar Toggle (Topbar) -->
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -18,16 +9,16 @@
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
-        
+
         <div class="topbar-divider d-none d-sm-block"></div>
-        
+
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= usuario()['nome'] ?></span>
                 <img class="img-profile rounded-circle" src="<?= arquivo('img/undraw_profile.svg') ?>">
             </a>
-            
+
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="<?= arquivo('modulos/perfil/perfil.php') ?>">
@@ -44,7 +35,7 @@
                 </a>
             </div>
         </li>
-        
+
     </ul>
-    
+
 </nav>
