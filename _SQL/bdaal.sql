85ca71544b3059244f9930308663da4b42f722f5
gilvanst <106268024+gilvanst@users.noreply.github.com>
2022-11-29 16:33:14 -0300

    banco att

--- _SQL/bdaal.sql
+++ _SQL/bdaal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Nov-2022 às 21:35
+-- Tempo de geração: 29-Nov-2022 às 20:29
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -41,7 +41,7 @@ CREATE TABLE `academicos` (
 --
 
 INSERT INTO `academicos` (`idAcad`, `nomeAcad`, `cadeiraAcad`, `posicaoAcad`, `sobreAcad`, `fotoAcad`) VALUES
-(23, '', '', 'rei do mundo', 'simplesmente o cara mais foda, amo ele ', '2511202207113741.jpg');
+(27, 'dsas', '242', '23', 'dfsvsc', 'dsas2911202208112302.jpg');
 
 -- --------------------------------------------------------
 
@@ -58,14 +58,6 @@ CREATE TABLE `eventos` (
   `descricaoEve` varchar(300) NOT NULL,
   `imagemEve` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `eventos`
---
-
-INSERT INTO `eventos` (`idEve`, `nomeEve`, `localEve`, `dataEve`, `horaEve`, `descricaoEve`, `imagemEve`) VALUES
-(19, 'AADSASDA', 'ASDASDA', '0000-00-00', '03:21:00', '1ASDCAS DADSDSAXDSAXSAZDCA', 'AADSASDA0911202201115900.jpg'),
-(20, 'festa', 'sei la', '3233-03-22', '03:32:00', 'blabla.klklskalkd', 'festa1411202207115432.jpg');
 
 -- --------------------------------------------------------
 
@@ -100,13 +92,6 @@ CREATE TABLE `memorial` (
   `foto` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `memorial`
---
-
-INSERT INTO `memorial` (`id`, `nome`, `cadeira`, `posicao`, `sobre`, `foto`) VALUES
-(1, 'Josisvaldo Santos', '4', 'Patrono', '', 'Josisvaldo Santos2511202208113257.jpg');
-
 -- --------------------------------------------------------
 
 --
@@ -127,18 +112,6 @@ CREATE TABLE `obra` (
   `generoObra` varchar(20) NOT NULL,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `obra`
---
-
-INSERT INTO `obra` (`idObra`, `tituloObra`, `autoresObra`, `sinopseObra`, `imagemObra`, `isbnObra`, `anoObra`, `paginasObra`, `pdfObra`, `linkObra`, `generoObra`, `id_usuario`) VALUES
-(83, 'As vantagens de ser invisível', 'Sthephen Chobsky', '\"O LIVRO QUE INSPIROU O FILMENOVA EDIÇÃO COM TRECHO INÉDITO Manter-se à margem oferece uma única e passiva perspectiva. Mas, de uma hora para outra, sempre chega o momento de encarar a vida do centro dos holofotes. Mais íntimas do que um diário, as cartas de Charlie são estranhas e únicas, hilárias e devastadoras. Não se sabe onde ele mora. Não se sabe para quem ele escreve. Tudo o que se conhece é o mundo que ele compartilha com o leitor. Estar encurralado entre o desejo de viver sua vida e fugir dela o coloca num novo caminho através de um território inexplorado. Um mundo de primeiros encontros amorosos, dramas familiares e novos amigos. Um mundo de sexo, drogas e rock’n’roll, quando o que todo mundo quer é aquela música certa que provoca o impulso perfeito para se sentir infinito. A luta entre apatia e entusiasmo marca o fim da adolescência de Charlie nesta história divertida e ao mesmo tempo instigante.\"', 'As vantagens de ser invisível1010202208105903.jpg', '9786555320695', '2014', '293', 'As vantagens de ser invisível1010202208105903.pdf', 'https://www.amazon.com.br/VANTAGENS-SER-INVIS%C3%8DVEL-NOVA-EDI%C3%87%C3%83O/dp/6555320699', 'romance, drama', 15),
-(86, 'Harry Potter e a pedra filosofal', 'J.K rowlling', 'Harry Potter é um garoto cujos pais, feiticeiros, foram assassinados por um poderosíssimo bruxo quando ele ainda era um bebê. Ele foi levado, então, para a casa dos tios que nada tinham a ver com o sobrenatural. Pelo contrário. Até os 10 anos, Harry foi uma espécie de gata borralheira: maltratado pelos tios, herdava roupas velhas do primo gorducho, tinha óculos remendados e era tratado como um estorvo. No dia de seu aniversário de 11 anos, entretanto, ele parece deslizar por um buraco sem fundo, como o de Alice no país das maravilhas, que o conduz a um mundo mágico. Descobre sua verdadeira história e seu destino: ser um aprendiz de feiticeiro até o dia em que terá que enfrentar a pior força do mal, o homem que assassinou seus pais. O menino de olhos verde, magricela e desengonçado, tão habituado à rejeição, descobre, também, que é um herói no universo dos magos. Potter fica sabendo que é a única pessoa a ter sobrevivido a um ataque do tal bruxo do mal e essa é a causa da marca em forma de raio que ele carrega na testa. Ele não é um garoto qualquer, ele sequer é um feiticeiro qualquer ele é Harry Potter, símbolo de poder, resistência e um líder natural entre os sobrenaturais. A fábula, recheada de fantasmas, paredes que falam, caldeirões, sapos, unicórnios, dragões e gigantes, não é, entretanto, apenas um passatempo.', 'Harry Potter e a pedra filosofal1710202205102033.jpg', '345644563456', '2004', '208', 'Harry Potter e a pedra filosofal1710202205102033.pdf', 'https://www.amazon.com.br/Harry-Potter-Pedra-Filosofal-Rowling/dp/8532530788', 'aventura', 15),
-(87, 'O pequeno príncipe', 'Antoíne de Saint-Exupery', 'Nesta clássica história que marcou gerações de leitores em todo o mundo, um piloto cai com seu avião no deserto do Saara e encontra um pequeno príncipe, que o leva a uma jornada filosófica e poética através de planetas que encerram a solidão humana. A edição conta com a clássica tradução do poeta imortal dom Marcos Barbosa, e é a versão mais consagrada da obra, publicada no Brasil desde 1952.', 'O pequeno príncipe1710202210101058.jpg', '8595081514', '2018', '96', 'O pequeno príncipe1710202210101058.pdf', 'https://www.amazon.com.br/pequeno-pr%C3%ADncipe-Antoine-Saint-Exup%C3%A9ry/dp/8595081514', 'ficção ciêntifica', 16),
-(88, 'Memórias póstumas de Brás Cubas', 'Machado De Assis', 'Não tive filhos, não transmiti a nenhuma criatura o legado da nossa miséria. Com essas palavras, o narrador de Memórias Póstumas de Brás Cubas resume a sua vida. O tom assumido na obra, bem como as técnicas empregadas na composição romanesca, são alguns dos fatores que justificam o lugar de Machado de Assis entre os maiores escritores do século XIX. Neste romance repleto de digressões filosóficas, o escritor se vale da posição privilegiada de Brás Cubas, que, como defunto autor, narra as suas desventuras e revela as contradições da sociedade brasileira do século XIX, por meio de uma análise aprofundada de seus personagens.', 'Memórias póstumas de Brás Cubas1810202206103036.jpg', '9788594318619', '1881', '192', 'Memórias póstumas de Brás Cubas1810202206103036.pdf', 'https://www.amazon.com.br/Mem%C3%B3rias-P%C3%B3stumas-Cubas-Machado-Assis/dp/8594318618/ref=d_pd_sbs_sccl_2_2/131-3698504-9154808?pd_rd_w=2JWqM&content-id=amzn1.sym.077f096c-3881-4160-87ce-2fdca6e1052', 'Literatura clássica', 15),
-(89, 'A metamorfose', 'Franz Kafka', 'A metamorfose é a mais célebre novela de Franz Kafka e uma das mais importantes de toda a história da literatura. Sem a menor cerimônia, o texto coloca o leitor diante de um caixeiro-viajante - o famoso Gregor Samsa - transformado em inseto monstruoso. A partir daí, a história é narrada com um realismo inesperado que associa o inverossímil e o senso de humor ao que é trágico, grotesco e cruel na condição humana - tudo no estilo transparente e perfeito desse mestre inconfundível da ficção universal.\r\n', 'A metamorfose1810202206104543.jpg', '139788571646858', '1915', '104', 'A metamorfose1810202206104543.pdf', 'https://www.amazon.com.br/metamorfose-Franz-Kafka/dp/8571646856', 'Ficçao absurdista', 15),
-(90, 'Aurora ', 'Friedrich Nietizche', 'Publicado em 1881, Aurora prossegue no estilo aforístico da filosofia de Friedrich Nietzsche (1844-1900), inaugurado com Humano, demasiado humano (1878). Em 575 aforismos - cuja extensão varia de duas linhas a algumas páginas -, Nietzsche elabora sua crítica da moral cristã-ocidental e dos conceitos a ela associados, como \"alma\", \"Deus\", \"pecado\", \"sujeito\" e \"livre-arbítrio\", que segundo o filósofo seriam ficções prejudiciais à vida. No subtítulo do livro, a palavra \"preconceito\" é usada no sentido filosófico de concepção formada antes do julgamento (um \"pré-juízo\"). A epígrafe (\"Há tantas auroras que não brilharam ainda\", extraído das escrituras hindus) explica o título e traduz a esperança nietzscheana de um mundo novo, livre das ilusões religiosas, morais e intelectuais criticadas pelo filósofo. Essa edição inclui o importante prólogo acrescentado em 1886 e, num apêndice bilíngüe, o grupo de poemas intitulados \"Idílios de Messina\", de 1882.', 'Aurora 1810202206105809.jpg', '8535928189', '1881', '312', 'Aurora 1810202206105809.pdf', 'https://www.amazon.com.br/Aurora-Friedrich-Nietzsche/dp/8535928189', 'Filosofia', 15);
 
 -- --------------------------------------------------------
 
@@ -179,14 +152,6 @@ CREATE TABLE `revista` (
   `ano` varchar(5) NOT NULL,
   `pdf` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `revista`
---
-
-INSERT INTO `revista` (`id`, `edicao`, `ano`, `pdf`) VALUES
-(3, '12', '2022', '121811202207114609.pdf'),
-(4, '11', '2021', '112211202207114528.pdf');
 
 -- --------------------------------------------------------
 
@@ -230,7 +195,9 @@ CREATE TABLE `videos` (
 --
 
 INSERT INTO `videos` (`id`, `nome`, `sobre`, `link`) VALUES
-(3, 'fut', 'dasljdslsajdsa', 'https://www.youtube.com/watch?v=ntKeSghZyag&ab_channel=iLance7i');
+(3, 'fut', 'dasljdslsajdsa', 'https://www.youtube.com/watch?v=ntKeSghZyag&ab_channel=iLance7i'),
+(4, 'Joji - 1AM FREESTYLE', 'melhor musica do album SMITHEREENS', 'https://www.youtube.com/watch?v=QTxMAycHnos&ab_channel=Joji'),
+(5, 'Joji - Nectar (Full Album)', 'album lindo', 'https://www.youtube.com/watch?v=5eDk-kTE9DI&ab_channel=SOULDEEP');
 
 --
 -- Índices para tabelas despejadas
@@ -298,13 +265,13 @@ ALTER TABLE `videos`
 -- AUTO_INCREMENT de tabela `academicos`
 --
 ALTER TABLE `academicos`
-  MODIFY `idAcad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `idAcad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de tabela `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `idEve` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `idEve` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de tabela `historia`
@@ -346,7 +313,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `videos`
 --
 ALTER TABLE `videos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
